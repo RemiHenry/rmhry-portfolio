c58949b10e50ab15a3056305a4b61a1feb91cb23
RemiHenry <remi.henry@epitech.eu>
2025-06-23 17:35:42 +0200

    fix: location undefined

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,15 @@ import Footer from "../components/footer/Footer";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import bg from "../images/bg.png";
+import { usePathname } from "next/navigation";
 
 function MyApp({ Component, pageProps }) {
+  const pathname = usePathname();
+
   return (
     <div
       style={{
-        backgroundImage: location.pathname === "/" ? `url(${bg.src})` : "",
+        backgroundImage: pathname === "/" ? `url(${bg.src})` : "",
         backgroundSize: "cover",
       }}
     >
